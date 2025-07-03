cdd136c3b4c343bfc164332eeef3f12cda82df1d
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-07-03 11:04:39 +0200

    init: miles app

---
+++ mfe/miles/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+   standalone: true,
+   imports: [],
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+   title = 'miles';
+}
+
+export default AppComponent;
