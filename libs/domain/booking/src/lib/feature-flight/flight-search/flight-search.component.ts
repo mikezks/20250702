75d8de6636e1eb92fc3d639d4a428b6902f5b849
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-07-04 11:12:52 +0200

    demo: signal update behavior, glitch-free behavior

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -35,6 +35,28 @@ export class FlightSearchComponent {
   constructor() {
     effect(() => console.log(this.route()));
     effect(() => this.search());
+
+    // Writable Signal update behavior
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Amsterdam' }));
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Madrid' }));
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Rome' }));
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Barcelona' }));
+    console.log(this.filter().from);
+    this.filter.update(curr => ({ ...curr, from: 'Oslo' }));
+    console.log(this.filter().from);
+
+    // Glitch-free behavior
+    const counter = signal(0);
+    const isEven = computed(() => counter() % 2 === 0);
+    effect(() => console.log({
+      counter: counter(),
+      isEven: isEven()
+    }));
+    setInterval(() => counter.update(curr => curr + 1), 1_000);
   }
 
   protected search(): void {
